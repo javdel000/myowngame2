893ec9fdad6ea9fb1124168b073fc39f0415fda8
javdel000 <58610616+javdel000@users.noreply.github.com>
2020-01-17 14:00:42 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -114,6 +114,113 @@ f f f f f f f f f f f f f f f f
 f f f f f f f f f f f f f f f f 
 `
 }
+function background () {
+    scene.setTileMap(img`
+f f f f f f f f f f f f f f f f f f f f f f f f 
+f 2 2 f b b b b f c c c c 5 c c c c f f f f f f 
+f 2 2 c b b c b f c f f f f f f f c f a 5 a f f 
+f f b c c c c b f c c c c c c c c c c 5 5 5 f f 
+f b b c b b b b f f c c c c c c c c a a 5 a f f 
+f b b c b b b b b f f c c c c c c c 5 a c f f f 
+f b c c b c b b b b b c c c c c c c c c c c c f 
+f b b b b c b b b b b b c c c c c c c c f f c f 
+f c c c c b b b e b b b b f c c c c c f c c c f 
+f b b b b b b b b b b b b f f c c c c f c c c f 
+f b b e b b b b e b b b b b f f c c c f c c 5 f 
+f b b b b b b b b b b b b b b b c c c f c c c f 
+f b e c c b b b b b b b b b b b b c c c f c c f 
+f b b e b b f f f b b b b b b b b b f c c f c f 
+f b b b b b f b f f b b b e b b c b f f c c c f 
+f b b b b b b b 9 f f b b b b b b e b f f f f f 
+f b e b b b f b b b f b b b b b b b b b b b b f 
+f b b b b b f f b f f b b b b b b b b b c c b f 
+f b b b e b b b b b b b b b b b b b e e c b b f 
+f b b b b b b b b b b b b b b c b b b e c b b f 
+f b c b b b b b b b b b b e b c b c c c c b f f 
+f b b b b b b b e c b b b b b c b c c b b 2 2 f 
+f b b b b b b b b b c b b b b c b b b b f 2 2 f 
+f f f f f f f f f f f f f f f f f f f f f f f f 
+`, TileScale.ThirtyTwo)
+    scene.setTile(15, img`
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+`, true)
+}
+function villian1 () {
+    villian = sprites.create(img`
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . f f f f f f . . . . . . . . . . . . . . 
+. . . . . . . . . . f f 8 8 5 5 8 8 f f . . . . . . . . . . . . 
+. . . . . . . . . f 8 8 8 8 5 5 8 8 8 8 f . . . . . . . . . . . 
+. . . . . . . . f 8 8 8 8 8 5 5 8 8 8 8 8 f . . . . . . . . . . 
+. . . . . . . . f 8 8 8 8 8 5 5 8 8 8 8 8 f . . . . . . . . . . 
+. . . . . . . f 8 5 8 8 8 8 5 5 8 8 8 8 5 8 f . . . . . . . . . 
+. . . . . . . f 8 5 5 5 8 8 5 5 8 8 5 5 5 8 f . . . . . . . . . 
+. . . . . . f 8 8 8 5 5 5 5 5 5 5 5 5 5 8 8 8 f . . . . . . . . 
+. . . . . . f 8 8 8 5 5 5 5 5 5 5 5 5 5 8 8 8 f . . . . . . . . 
+. . . . . . f 8 8 5 5 5 5 5 5 5 5 5 5 5 5 8 8 f . . . . . . . . 
+. . . . . . f 8 5 a a a a a a a a a a a a 5 8 f . . . . . . . . 
+. . . . . . f 8 5 a f 1 a a a a a a f 1 a 5 8 f . . . . . . . . 
+. . . . . . . f 5 a f f a a a a a a f f a 5 f . . . . . . . . . 
+. . . . . . . f 5 a a a a a a a a a a a a 5 f . . . . . . . . . 
+. . . . . . f 5 f 5 a 1 1 1 1 1 1 1 1 a 5 f 5 f . . . . . . . . 
+. . . . . f 5 5 5 f a a a a a a a a a a f 5 5 5 f . . . . . . . 
+. . . . . f f 5 5 5 f f f f f f f f f f 5 5 5 f f . . . . . . . 
+. . . . . . . f 8 8 5 5 5 5 5 5 5 5 5 5 8 8 f . . . . . . . . . 
+. . . . . . f 8 8 f 8 8 5 5 5 5 5 5 8 8 f 8 8 f f . . . . . . . 
+. . . . . f 5 5 5 f 8 8 8 8 5 5 8 8 8 8 f 5 5 5 5 f . . . . . . 
+. . . . . f 5 5 f f 5 5 5 5 5 5 5 5 5 5 f f 4 2 8 f . . . . . . 
+. . . . . . f f . f 5 5 5 8 5 5 8 5 5 5 f . f 7 3 f . . . . . . 
+. . . . . . . . . f 8 8 8 8 5 5 8 8 8 8 f . . f f . . . . . . . 
+. . . . . . . . f 8 8 8 8 f f f f 8 8 8 8 f . . . . . . . . . . 
+. . . . . . . . f 5 5 5 f . . . . f 5 5 5 f . . . . . . . . . . 
+. . . . . . . f f 5 5 5 f . . . . f 5 5 5 f f . . . . . . . . . 
+. . . . . . f 5 5 5 5 5 f . . . . f 5 5 5 5 5 f . . . . . . . . 
+. . . . . . f 5 5 5 5 5 f . . . . f 5 5 5 5 5 f . . . . . . . . 
+. . . . . . f f f f f f f . . . . f f f f f f f . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+`, SpriteKind.Enemy)
+    villian.setPosition(658, 110)
+    villian.follow(hero, 100)
+}
+function countdown () {
+    info.startCountdown(500)
+}
+function Ammo () {
+    Ammo2 = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . b b b b b . . 
+. . . . . . . . b b 7 7 7 b b . 
+. . . . . . . b b 7 7 7 7 7 b . 
+. . . . . . b b 7 7 7 7 7 7 b . 
+. . . . . b b 7 7 7 7 7 7 7 b . 
+. . . . . b 7 7 7 7 7 7 7 b b . 
+. . . . . b 7 7 7 7 7 7 b b . . 
+. . . . . b 7 7 7 7 7 b b . . . 
+. . . . . b b 7 7 7 b b . . . . 
+. . . . . . b b b b b . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.Food)
+}
 function hero2 () {
     hero = sprites.create(img`
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
@@ -150,41 +257,7 @@ function hero2 () {
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
 `, SpriteKind.Player)
     controller.moveSprite(hero, 200, 200)
-    hero.setPosition(69, 73)
-    villian = sprites.create(img`
-. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . f f f f f f . . . . . . . . . . . . . . 
-. . . . . . . . . . f f 8 8 5 5 8 8 f f . . . . . . . . . . . . 
-. . . . . . . . . f 8 8 8 8 5 5 8 8 8 8 f . . . . . . . . . . . 
-. . . . . . . . f 8 8 8 8 8 5 5 8 8 8 8 8 f . . . . . . . . . . 
-. . . . . . . . f 8 8 8 8 8 5 5 8 8 8 8 8 f . . . . . . . . . . 
-. . . . . . . f 8 5 8 8 8 8 5 5 8 8 8 8 5 8 f . . . . . . . . . 
-. . . . . . . f 8 5 5 5 8 8 5 5 8 8 5 5 5 8 f . . . . . . . . . 
-. . . . . . f 8 8 8 5 5 5 5 5 5 5 5 5 5 8 8 8 f . . . . . . . . 
-. . . . . . f 8 8 8 5 5 5 5 5 5 5 5 5 5 8 8 8 f . . . . . . . . 
-. . . . . . f 8 8 5 5 5 5 5 5 5 5 5 5 5 5 8 8 f . . . . . . . . 
-. . . . . . f 8 5 a a a a a a a a a a a a 5 8 f . . . . . . . . 
-. . . . . . f 8 5 a f 1 a a a a a a f 1 a 5 8 f . . . . . . . . 
-. . . . . . . f 5 a f f a a a a a a f f a 5 f . . . . . . . . . 
-. . . . . . . f 5 a a a a a a a a a a a a 5 f . . . . . . . . . 
-. . . . . . f 5 f 5 a 1 1 1 1 1 1 1 1 a 5 f 5 f . . . . . . . . 
-. . . . . f 5 5 5 f a a a a a a a a a a f 5 5 5 f . . . . . . . 
-. . . . . f f 5 5 5 f f f f f f f f f f 5 5 5 f f . . . . . . . 
-. . . . . . . f 8 8 5 5 5 5 5 5 5 5 5 5 8 8 f . . . . . . . . . 
-. . . . . . f 8 8 f 8 8 5 5 5 5 5 5 8 8 f 8 8 f f . . . . . . . 
-. . . . . f 5 5 5 f 8 8 8 8 5 5 8 8 8 8 f 5 5 5 5 f . . . . . . 
-. . . . . f 5 5 f f 5 5 5 5 5 5 5 5 5 5 f f 4 2 8 f . . . . . . 
-. . . . . . f f . f 5 5 5 8 5 5 8 5 5 5 f . f 7 3 f . . . . . . 
-. . . . . . . . . f 8 8 8 8 5 5 8 8 8 8 f . . f f . . . . . . . 
-. . . . . . . . f 8 8 8 8 f f f f 8 8 8 8 f . . . . . . . . . . 
-. . . . . . . . f 5 5 5 f . . . . f 5 5 5 f . . . . . . . . . . 
-. . . . . . . f f 5 5 5 f . . . . f 5 5 5 f f . . . . . . . . . 
-. . . . . . f 5 5 5 5 5 f . . . . f 5 5 5 5 5 f . . . . . . . . 
-. . . . . . f 5 5 5 5 5 f . . . . f 5 5 5 5 5 f . . . . . . . . 
-. . . . . . f f f f f f f . . . . f f f f f f f . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
-`, SpriteKind.Enemy)
+    hero.setPosition(272, 495)
 }
 function camera () {
     scene.cameraFollowSprite(hero)
@@ -210,75 +283,12 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 `, hero, 100, 0)
 })
 let projectile: Sprite = null
-let villian: Sprite = null
+let Ammo2: Sprite = null
 let hero: Sprite = null
+let villian: Sprite = null
 hero2()
 camera()
-scene.setTileMap(img`
-f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b f 
-f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
-`, TileScale.ThirtyTwo)
-scene.setTile(15, img`
-f f f f f f f f f f f f f f f f 
-f f f f f f f f f f f f f f f f 
-f f f f f f f f f f f f f f f f 
-f f f f f f f f f f f f f f f f 
-f f f f f f f f f f f f f f f f 
-f f f f f f f f f f f f f f f f 
-f f f f f f f f f f f f f f f f 
-f f f f f f f f f f f f f f f f 
-f f f f f f f f f f f f f f f f 
-f f f f f f f f f f f f f f f f 
-f f f f f f f f f f f f f f f f 
-f f f f f f f f f f f f f f f f 
-f f f f f f f f f f f f f f f f 
-f f f f f f f f f f f f f f f f 
-f f f f f f f f f f f f f f f f 
-f f f f f f f f f f f f f f f f 
-`, true)
+background()
+villian1()
+countdown()
+Ammo()
